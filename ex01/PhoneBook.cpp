e278131439a08d15e3bdb0f2576b22a93152888a
Luca De Micheli <lde-mich@e4r6p2.42roma.it>
2023-10-06 11:03:29 +0200

    update-oct-6

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,29 +6,39 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 15:57:52 by lde-mich          #+#    #+#             */
-/*   Updated: 2023/10/05 16:12:47 by lde-mich         ###   ########.fr       */
+/*   Updated: 2023/10/06 11:02:41 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
+PhoneBook::PhoneBook()
+{
+    
+}
+
+PhoneBook::~PhoneBook()
+{
+    
+}
+
 void PhoneBook::add()
 {
     std::string temp;
 
-    std::cout<<"Insert firstName"<<std::endl;
+    std::cout << "Insert firstName" << std::endl;
     std::getline (std::cin, temp);
     this->rubrica[0].setFirstName(temp);
-    std::cout<<"Insert lastName"<<std::endl;
+    std::cout << "Insert lastName" << std::endl;
     std::getline (std::cin, temp);
     this->rubrica[0].setLastName(temp);
-    std::cout<<"Insert nickName"<<std::endl;
+    std::cout << "Insert nickName" << std::endl;
     std::getline (std::cin, temp);
     this->rubrica[0].setNickName(temp);
-    std::cout<<"Insert phoneNumber"<<std::endl;
+    std::cout << "Insert phoneNumber" << std::endl;
     std::getline (std::cin, temp);
     this->rubrica[0].setPhoneNumber(temp);
-    std::cout<<"Insert darkestSecret"<<std::endl;
+    std::cout << "Insert darkestSecret" << std::endl;
     std::getline (std::cin, temp);
     this->rubrica[0].setDarkestSecret(temp);
 }
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 15:57:00 by lde-mich          #+#    #+#             */
-/*   Updated: 2023/10/04 14:47:29 by lde-mich         ###   ########.fr       */
+/*   Updated: 2023/10/06 10:58:44 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ class   PhoneBook
         Contact rubrica[8];
     public:
         void add();
-        void search();
+        // void search();
         PhoneBook();
+        ~PhoneBook();
 };
